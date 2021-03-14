e20f7bfe73ac055f194be12f62406c499f45b776
Zach Berwaldt <zberwaldt@tutamail.com>
2021-03-13 22:54:02 -0500

    tweaks to weback config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     devServer: {
         contentBase: './dist',
@@ -14,7 +17,13 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
-            },
+            }, {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }, {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
         ],
     },
 };
